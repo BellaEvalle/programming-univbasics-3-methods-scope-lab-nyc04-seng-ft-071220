a7e07ee276fc141bcce5a24b136465fc8e057dd0
Bella Evalle <bellaevalle@yahoo.com>
2020-07-15 22:58:33 +0000

    Automatically backed up by Learn

---
+++ lib/catch_phrases.rb
@@ -0,0 +1,16 @@
+def mario
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts phrase
+end
+
+def toadstool
+  puts status
+end
+
+def link
+  puts "It's Dangerous To Go Alone! Take This."
+end
+
+def all_phrases
+  puts phrase
+end
